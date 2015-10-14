0e75e6f162bb0283d34fc533bd30d97a5180dbcd
mfernandez82 <miguel.fernandez1982@yahoo.cl>
2015-10-14 11:09:40 -0300

    ultimas modificaciones

--- Actividad6Tests/Actividad6Tests.swift
+++ Actividad6Tests/Actividad6Tests.swift
@@ -54,6 +54,7 @@ class Actividad6Tests: XCTestCase {
             
                   ]
         println(revisar)
+        dump(revisar)
        
     }
     
@@ -77,10 +78,44 @@ class Actividad6Tests: XCTestCase {
     }
     
    
-    func testPerformanceOrdenar(arreglo: NSString){
+    func testPerformanceOrdenar(){
         self.measureBlock(){
         // ordenando un arreglo utilizando el metodo dump
-            revisar
+          
+            
+            // creando un arreglo (revisar), en donde se guardaran los usuarios
+            var revisar = [Usuarios]()
+            //para crear clientes
+            var clientes = [Clientes]()
+            
+            
+            // creando los clientes, que despues se asociaran al usuario que se logea, solo para el caso de prueba
+            let juan = Clientes(nombre_cliente: "juan carlos", direccion: "la costa 40")
+            let gustavo =  Clientes(nombre_cliente: "Gustavo fladez", direccion: "ramirez 1050")
+            let victor = Clientes(nombre_cliente: "Victor inzunza", direccion: "Bilbao 201")
+            let jose = Clientes(nombre_cliente: "jose asenjo", direccion: "matta 5020")
+            let sammy = Clientes(nombre_cliente: "Sammanta ricouz", direccion: "errazuriz 1020")
+            let marta = Clientes(nombre_cliente: "Marta rizz", direccion: "costa 40")
+            let jorge =  Clientes(nombre_cliente: "jorge gomez", direccion: "no tengo idea")
+            let carlos =  Clientes(nombre_cliente: "carlos santana", direccion: "ddddasd ")
+            let paola = Clientes(nombre_cliente: "paola sanchez", direccion: "repulci 1050")
+            
+            
+            // guardando en un arreglo 10 usuarios, para su posterior ordenamiento
+            revisar += [Usuarios(nombre: "user", clave: "123", cliente: [(carlos,9999)]),
+                Usuarios(nombre: "user2", clave: "234", cliente: [ (jorge,777),(paola,4444)]),
+                Usuarios(nombre: "juan", clave: "juan", cliente: [(gustavo,12333)]),
+                Usuarios(nombre: "victor", clave: "victor", cliente: [(victor,12333)]),
+                Usuarios(nombre: "andres", clave: "andres", cliente: [(jose,12333)]),
+                Usuarios(nombre: "marta", clave: "marta", cliente: [(sammy,12333)]),
+                Usuarios(nombre: "bernardo", clave: "bernado", cliente: [(marta,12333)]),
+                Usuarios(nombre: "carlos", clave: "carlos", cliente: [(paola,12333)]),
+                Usuarios(nombre: "venito", clave: "venito", cliente: [(carlos,12333)]),
+                Usuarios(nombre: "paola", clave: "paola", cliente: [(juan,12333)])
+                
+            ]
+            println(revisar)
+            dump(revisar)
             
         }
         
